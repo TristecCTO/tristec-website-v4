54e5491dd3b6cb340aba2f2be0aa5a934c3f2de2
TristecCTO <tristan.simpson@tristec.co.uk>
2025-03-04 17:36:21 +0000

    AVIF off

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,15 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: ["auto", "webp"], // Explicitly exclude AVIF
+          quality: 50,
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -32,13 +40,9 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
-}
-
+};
