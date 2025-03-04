efc92ee65f11c74b90c43c90a5265e2382a9a90b
TristecCTO <tristan.simpson@tristec.co.uk>
2025-03-04 19:06:05 +0000

    Tristec Ltd title

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Tristec Ltd`,
+    description: `Tristec Ltd website. `,
+    author: `@tristeccto`,
+    siteUrl: `https://tristec.co.uk/`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -43,7 +43,7 @@ module.exports = {
         name: `Tristec Ltd`,
         short_name: `tristec`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#663399`, // was `#663399`,
         display: `minimal-ui`,
         icon: `src/images/tristec-icon.png`,
       },
